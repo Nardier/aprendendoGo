4c8e0ebc3fe4b29ea8f5c1873f4084391c0d1355
Nardier <nardierbarbosa@gmail.com>
2019-04-04 17:39:01 -0300

    Algumas opçoes

--- Main.go
+++ Main.go
@@ -3,12 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	var livraria [5]int
+	var livraria [5]string
+	livraria[0] = "Nardier"
+	livraria[1] = "Barbosa"
+	livraria[2] = "de"
+	livraria[3] = "Lira"
+	livraria[4] = "Sampaio"
+
+	//var ponteir [...]*string = &livraria[5]
 	opcaoMenu := menu()
 
 	switch opcaoMenu {
 	case 1:
 		primeiraOpcao(livraria)
+	case 2:
+		segundaOpcao(livraria)
+	case 4:
+		quartaOpcao(livraria)
 	default:
 	}
 }
@@ -30,6 +41,40 @@ func menu() int {
 	return menuOpcao
 }
 
-func primeiraOpcao(vet [5]int) {
-	fmt.Println("Ok")
+func primeiraOpcao(vet [5]string) {
+	var posicaoLivro int
+	fmt.Println("Selecione a posição do livro de entre 1 e 5")
+	fmt.Scan(&posicaoLivro)
+	posicaoLivro = posicaoLivro - 1
+
+	if posicaoLivro > 5 || posicaoLivro < 0 {
+		fmt.Println("Opção inválida")
+	} else {
+		fmt.Println("Nº:", posicaoLivro+1, "Livro ->", vet[posicaoLivro])
+	}
+
+}
+
+func segundaOpcao(vet [5]string) {
+	for j := 0; j < 5; j++ {
+		fmt.Println("Nº:", j, "Livro ->", vet[j])
+	}
+}
+
+func quartaOpcao(vet [5]string) {
+	var posicaoLivro int
+	var novoLivro string
+	fmt.Println("Selecione a posição do livro de entre 1 e 5 para sobrescrever")
+	fmt.Scan(&posicaoLivro)
+	posicaoLivro = posicaoLivro - 1
+
+	if posicaoLivro > 5 || posicaoLivro < 0 {
+		fmt.Println("Opção inválida")
+	} else {
+		fmt.Println("Digite o novo nome do livro")
+		fmt.Scan(&novoLivro)
+		vet[posicaoLivro] = novoLivro
+		fmt.Println("Alterado com sucesso")
+	}
+	segundaOpcao(vet)
 }
